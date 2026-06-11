e7653ccd7bc4733b9b261216bcf40eb2153fa6a9
Guilherme Couto <coutog754@gmail.com>
2026-06-11 00:29:58 +0000

    Acrescentei a variável total

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidade
-  int n;
+  int n, total;
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
